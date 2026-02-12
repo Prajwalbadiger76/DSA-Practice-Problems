36d648b5a45a82fee007426cd162f28ce1e1df92
Prajwal Badiger <prajwalbadiger7676@gmail.com>
2026-02-13 00:09:39 +0530

    Added Missing Number Problem

---
+++ src/Array_Problems/MissingNumber.java
@@ -0,0 +1,33 @@
+package Array_Problems;
+
+public class MissingNumber {
+
+    public static int missingNumber(int[] nums) {
+
+        int n = nums.length;
+
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+
+    public static void main(String[] args) {
+
+        // Test Case 1
+        int[] nums1 = {3, 0, 1};
+        System.out.println(missingNumber(nums1)); // Output: 2
+
+        // Test Case 2
+        int[] nums2 = {0, 1};
+        System.out.println(missingNumber(nums2)); // Output: 2
+
+        // Test Case 3
+        int[] nums3 = {9,6,4,2,3,5,7,0,1};
+        System.out.println(missingNumber(nums3)); // Output: 8
+    }
+}
