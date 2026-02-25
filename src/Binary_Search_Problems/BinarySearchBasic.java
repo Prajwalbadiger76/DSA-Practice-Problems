23a411b0c81387bd3bfc01b92e144972af845967
Prajwal Badiger <prajwalbadiger7676@gmail.com>
2026-02-25 23:27:59 +0530

    Added basic Binary Search Problem

---
+++ src/Binary_Search_Problems/BinarySearchBasic.java
@@ -0,0 +1,30 @@
+package Binary_Search_Problems;
+
+public class BinarySearchBasic {
+
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } 
+            else if (arr[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9};
+        System.out.println(binarySearch(arr, 7)); // Output: 3
+    }
+}
