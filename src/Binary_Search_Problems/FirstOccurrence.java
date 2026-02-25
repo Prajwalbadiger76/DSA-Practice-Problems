f431e7b854b05ce131e6987b9f6b28fdb0691c28
Prajwal Badiger <prajwalbadiger7676@gmail.com>
2026-02-25 23:29:31 +0530

    Added First Occurrence using Binary Search Problem

---
+++ src/Binary_Search_Problems/FirstOccurrence.java
@@ -0,0 +1,27 @@
+package Binary_Search_Problems;
+
+public class FirstOccurrence {
+
+    public static int findFirst(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                result = mid;
+                high = mid - 1; // Move left
+            } 
+            else if (arr[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
