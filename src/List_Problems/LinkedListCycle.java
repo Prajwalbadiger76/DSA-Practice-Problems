75a832206eebe104bc7bd849edeae050e37c2018
Prajwal Badiger <prajwalbadiger7676@gmail.com>
2026-02-26 23:42:06 +0530

    Added Linked List Cycle Problem

---
+++ src/List_Problems/LinkedListCycle.java
@@ -0,0 +1,55 @@
+package List_Problems;
+
+public class LinkedListCycle {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public static boolean hasCycle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        // Create nodes
+        ListNode n1 = new ListNode(3);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(0);
+        ListNode n4 = new ListNode(-4);
+
+        // Link nodes
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+
+        // Create cycle (tail connects to node 2)
+        n4.next = n2;
+
+        System.out.println(hasCycle(n1)); // true
+    }
+}
