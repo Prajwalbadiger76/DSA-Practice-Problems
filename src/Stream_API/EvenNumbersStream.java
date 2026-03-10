1c1f54ae50cb372e8c37bd78c3996729f9ff0027
Prajwal Badiger <prajwalbadiger7676@gmail.com>
2026-03-10 20:05:24 +0530

    Added Even Numbers Stream

---
+++ src/Stream_API/EvenNumbersStream.java
@@ -0,0 +1,14 @@
+package Stream_API;
+
+import java.util.*;
+
+public class EvenNumbersStream {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30, 35);
+
+        numbers.stream()
+               .filter(n -> n % 2 == 0)
+               .forEach(System.out::println);
+    }
+}
